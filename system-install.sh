f9db6d72c7d27d0c82d74b13b1933b3ad83f9f42
Théo FORTIN <38886040+topiga@users.noreply.github.com>
2024-05-15 20:01:36 +0200

    Update system-install.sh
    RoundWaita

--- system-install.sh
+++ system-install.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/bash
 
-sudo mkdir -p /usr/share/icons/MoreWaita/
+sudo mkdir -p /usr/share/icons/RoundWaita/
 shopt -s extglob
-sudo cp -avu "$(pwd -P)"/!(*.build|*.sh|*.py|*.md|.git|.github|.gitignore|_dev) /usr/share/icons/MoreWaita/
+sudo cp -avu "$(pwd -P)"/!(*.build|*.sh|*.py|*.md|.git|.github|.gitignore|_dev) /usr/share/icons/RoundWaita/
 shopt -u extglob
-sudo find /usr/share/icons/MoreWaita/ -name '*.build' -type f -delete
-sudo gtk-update-icon-cache -f -t /usr/share/icons/MoreWaita && xdg-desktop-menu forceupdate
+sudo find /usr/share/icons/RoundWaita/ -name '*.build' -type f -delete
+sudo gtk-update-icon-cache -f -t /usr/share/icons/RoundWaita && xdg-desktop-menu forceupdate
