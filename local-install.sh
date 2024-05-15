7958c8ce95347760782f37636934b9541d8ca8d2
Théo FORTIN <38886040+topiga@users.noreply.github.com>
2024-05-15 20:01:16 +0200

    Update local-install.sh
    RoundWaita

--- local-install.sh
+++ local-install.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/sh
 
-mkdir -p ~/.local/share/icons/MoreWaita/
+mkdir -p ~/.local/share/icons/RoundWaita/
 shopt -s extglob
-cp -avu "$(pwd -P)"/!(*.build|*.sh|*.py|*.md|.git|.github|.gitignore|_dev) ~/.local/share/icons/MoreWaita/
+cp -avu "$(pwd -P)"/!(*.build|*.sh|*.py|*.md|.git|.github|.gitignore|_dev) ~/.local/share/icons/RoundWaita/
 shopt -u extglob
-find ~/.local/share/icons/MoreWaita/ -name '*.build' -type f -delete
-gtk-update-icon-cache -f -t ~/.local/share/icons/MoreWaita && xdg-desktop-menu forceupdate
+find ~/.local/share/icons/RoundWaita/ -name '*.build' -type f -delete
+gtk-update-icon-cache -f -t ~/.local/share/icons/RoundWaita && xdg-desktop-menu forceupdate
 
